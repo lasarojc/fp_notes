47ed52bf83273d09686346d515fb8742e8bce61d
lasaro <lasaro@users.noreply.github.com>
2021-07-01 06:43:31 -0300

    Deployed 5436492 with MkDocs version: 1.1.2

--- javascripts/mathjaxhelper.js
+++ javascripts/mathjaxhelper.js
@@ -1,9 +1,11 @@
 window.MathJax = {
+  loader: {load: ['[tex]/ams']},
   tex: {
     inlineMath: [["\\(", "\\)"]],
     displayMath: [["\\[", "\\]"]],
     processEscapes: true,
-    processEnvironments: true
+    processEnvironments: true,
+    packages: {packages: {'[+]': ['ams']}}
   },
   options: {
     ignoreHtmlClass: ".*|",
