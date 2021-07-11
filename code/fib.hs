689ff47e895eef3e86bdd6611bd6fd4e473601c4
lasaro <lasaro@users.noreply.github.com>
2021-07-11 18:04:37 -0300

    Deployed af2725b with MkDocs version: 1.1.2

--- code/fib.hs
+++ code/fib.hs
@@ -2,15 +2,15 @@
 >>> fibIf 4
 3
 -}
-fibIf n = if n == 1 then 1
-            else if n == 2 then 1
-            else fibIf (n - 1) + fibIf (n - 2)
+fibIf n = if n == 1 then 0
+          else if n == 2 then 1
+          else fibIf (n - 1) + fibIf (n - 2)
 
 fibGuard n
-  | n == 1 = 1
+  | n == 1 = 0
   | n == 2 = 1
   | otherwise = fibGuard (n - 1) + fibGuard (n - 2)
 
-fibPattern 1 = 1
+fibPattern 1 = 0
 fibPattern 2 = 1
 fibPattern n = fibPattern (n - 1) + fibPattern (n - 2)
