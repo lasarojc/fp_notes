bc8ca25e561a3cfb2483936b20974b014dd617d7
lasaro <lasaro@users.noreply.github.com>
2021-07-28 10:37:23 -0300

    Deployed ed8a783 with MkDocs version: 1.1.2

--- code/scratch.hs
+++ code/scratch.hs
@@ -12,10 +12,46 @@ Indefinido
 "[]"
 
 >>>fat 5
-120
+30414093201713378043612608166064768844377641568960512000000000000
 -}
 
 fat 0 = 1
 fat n 
     |n > 0 = n * fat (n-1)
     |otherwise = error "Indefinido"
+
+
+
+{-
+>>>diasMes 5
+31
+
+>>>diasMes 5.1
+No instance for (Fractional Int) arising from the literal ‘5.1’
+
+>>>diasMes 37::Int
+30
+
+>>>diasMes "lala"
+Couldn't match expected type ‘Int’ with actual type ‘[Char]’
+-}
+
+diasMes :: Int -> Int
+diasMes m
+    | m == 1 || m == 3 || m == 5 || m == 7 || m == 8 || m == 10 || m == 12 = 31
+    | m == 2 = 28
+    | otherwise = 30
+
+
+
+
+{-
+>>>minhaFunção "entrada1" 2 2
+"tr"
+>>>minhaFunção "entrada1" 2 3
+"tra"
+-}
+
+minhaFunção :: String -> Int -> Int -> String
+minhaFunção s t n = reverse (take n (reverse (take (t + n) s)))
+
