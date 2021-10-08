d78b6d99699ac1ae564b10851a47b08b29c844c2
lasaro <lasaro@users.noreply.github.com>
2021-10-08 18:49:48 -0300

    Deployed c0e68f5 with MkDocs version: 1.2.2

---
+++ code/foldable.hs
@@ -0,0 +1,21 @@
+import Data.Foldable as F
+
+data Árvore a = Nada | Nó a (Árvore a) (Árvore a)
+    deriving (Show,Eq,Read)
+
+instance F.Foldable Árvore where
+    foldMap f Nada         = mempty
+    foldMap f (Nó a ae ad) = F.foldMap f ae   `mappend` 
+                             f a              `mappend` 
+                             F.foldMap f ad
+
+    foldr f acc  Nada        = acc
+    foldr f acc (Nó a ae ad) = foldr f (f a (foldr f acc ad)) ae
+
+{- 
+>>>arv = Nó 1    (Nó 2 (Nó 4 Nada Nada) Nada)     (Nó 3 Nada Nada)
+>>> foldr (+) 0 arv
+>>> F.foldl1 (+) arv
+>>>arv2 = Nó  "a"    (Nó "b" (Nó "c" Nada Nada) Nada)     (Nó "d" Nada Nada)
+>>> foldr (++)  "" arv2
+-}
