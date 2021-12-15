5c07307e7e92bcf741556fc8c132e70c0dcd06d3
lasaro <lasaro@here>
2021-12-15 10:27:01 -0300

    Deployed 3d99fcc with MkDocs version: 1.2.2

--- code/scratch2.hs
+++ code/scratch2.hs
@@ -36,3 +36,41 @@ maiorDeTres a b c
 éZero x
   | x < 0 = False 
   | x == 0 = True
+
+
+
+-- >>> soma2v (1,4) (3,4)
+-- (4,8)
+
+soma2v :: (Int,Int) -> (Int,Int) -> (Int,Int)
+soma2v        p1          p2    =
+   let p1x = fst p1 
+       p1y = snd p1
+       p2x = fst p2
+       p2y = snd p2
+       prx = p1x + p2x
+       pry = p1y + p2y
+   in (prx, pry)
+
+
+-- >>> soma2v' (1,4) (3,4)
+-- (4,8)
+
+soma2v' (x1,y1) (x2,y2) = (x1+x2, y1+y2)
+
+-- >>> prim (1,2,3,4)
+-- 2
+
+prim :: (a, b, c, d) -> b
+prim (_,x,_,_) =  x
+
+prim' :: (a,b,c) -> b
+prim' (_,x,_) =  x
+
+
+-- >>> f 1
+--(False, 1)
+
+-- >>> f (-1)
+--(True, 1)
+
